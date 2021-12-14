21b5455caea0e0b6628f91fb6b7981f88f109bde
Kristian Utsar <utsaru@gmail.com>
2021-12-14 22:52:25 +0000

    init 2

--- src/server.js
+++ src/server.js
@@ -1,8 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-const PORT = process.env.PORT || 3001;
+dotenv.config();
 
 const server = express();
-server.listen(PORT, () => {
-  console.log(`server is running on port: ${PORT}`);
+
+const PORT = process.env.PORT || 3001;
+const DATABASE_URI = process.env.MONGO_URI;
+
+server.use(cors());
+server.use(express.json());
+
+mongoose.connect(DATABASE_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  server.listen(PORT, () => {
+    console.log(`server is running on port: ${PORT}`);
+  });
 });
