e5c2a268a4144e4ba85f9fa7a053a047ea658067
Kristian Utsar <utsaru@gmail.com>
2021-12-16 14:49:03 +0000

    setting up routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+import usersRouter from "./routes/users/users.js";
+import listEndpoints from "express-list-endpoints";
+import authRouter from "./routes/auth/auth.js";
 
 dotenv.config();
 
@@ -10,8 +16,24 @@ const server = express();
 const PORT = process.env.PORT || 3001;
 const DATABASE_URI = process.env.MONGO_URI;
 
+// **********Middlewares**********
+
 server.use(cors());
 server.use(express.json());
+server.use(helmet());
+server.use(morgan("common"));
+
+// **********Routes**********
+server.use("/users", usersRouter);
+server.use("/auth", authRouter);
+
+// server.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
+
+console.table(listEndpoints(server));
+
+// **********Connect to MongoDB**********
 
 mongoose.connect(
   DATABASE_URI,
@@ -23,6 +45,7 @@ mongoose.connect(
     console.log("Connected to database");
   }
 );
+
 mongoose.connection.on("connected", () => {
   server.listen(PORT, () => {
     console.log(`server is running on port: ${PORT}`);
